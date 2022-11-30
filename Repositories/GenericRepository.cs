2f252db03efa45ec1159191489bd477598ab770c
unknown <ShokhrukhDeveloper>
2022-11-30 18:05:35 +0500

    I generic repository and Generic Repository readi

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+using System.Linq.Expressions;
+using ShoxShop.Data;
+
+namespace ShoxShop.Repositories;
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity :class
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public GenericRepository(ApplicationDbContext context)
+    {
+        _dbContext=context;
+    }
+    public async ValueTask<TEntity> AddAsync(TEntity entity)
+    {
+        var entry= await _dbContext.Set<TEntity>().AddAsync(entity);
+        await _dbContext.SaveChangesAsync();
+        return entry.Entity;
+    }
+
+    public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> expression)
+        =>_dbContext.Set<TEntity>().Where(expression);
+
+    public TEntity? GetById(ulong id)
+        =>  _dbContext.Set<TEntity>().Find(id);
+
+    public IQueryable<TEntity> GetEntities()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<TEntity> GetEntity()
+        =>_dbContext.Set<TEntity>();
+
+    public async ValueTask<TEntity> Remove(TEntity entity)
+    {
+        var entry = _dbContext.Set<TEntity>().Remove(entity);
+        await _dbContext.SaveChangesAsync();
+        return entry.Entity;
+    }
+
+    public async ValueTask<TEntity> Update(TEntity entity)
+    {
+        var entry = _dbContext.Set<TEntity>().Remove(entity);
+        await _dbContext.SaveChangesAsync();
+        return entry.Entity;
+    }
+}
---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+using System.Linq.Expressions;
+
+namespace ShoxShop.Repositories;
+public interface IGenericRepository<TEntity> where TEntity:class
+{
+    TEntity? GetById(ulong id);
+    IQueryable<TEntity> GetEntities();
+    IEnumerable<TEntity> Find(Expression<Func<TEntity,bool>> expression);
+    ValueTask<TEntity> AddAsync(TEntity entity);
+    ValueTask<TEntity> Remove(TEntity entity);
+    ValueTask<TEntity> Update(TEntity entity);
+}
