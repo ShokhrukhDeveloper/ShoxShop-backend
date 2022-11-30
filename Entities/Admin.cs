17cfe914ef0cddfb1a7de41c00b3bb3f91071508
unknown <ShokhrukhDeveloper>
2022-11-30 15:22:29 +0500

    Login admin configuration ready

--- Entities/Admin.cs
+++ Entities/Admin.cs
@@ -7,5 +7,6 @@ public class Admin:EntityBase
     public DateTime BirthDate { get; set; }
     public string Image { get; set; }
     public string PhoneNumber { get; set; }
+    public LoginAdmin AdminLogin { get; set; }
     public ICollection<Vendor> Vendors { get; set; }
 }
