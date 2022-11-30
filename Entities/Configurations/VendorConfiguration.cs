a94196414f91305af918f4f6722652437ca50da3
unknown <ShokhrukhDeveloper>
2022-11-30 10:09:07 +0500

    Vendor configuration

---
+++ Entities/Configurations/VendorConfiguration.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ShoxShop.Entities.Configurations;
+public class VendorConfiguration:ConfigurationBase<Vendor>
+{
+    public override void Configure(EntityTypeBuilder<Vendor> builder)
+    {
+        base.Configure(builder);
+        builder.HasKey(k=>k.VendorId);
+        builder.Property(p=>p.VendorId).ValueGeneratedOnAdd();
+        builder.HasMany(p=>p.VendorSessions)
+            .WithOne(q=>q.Vendor)
+            .HasForeignKey(k=>k.VendorId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+    }
+}
